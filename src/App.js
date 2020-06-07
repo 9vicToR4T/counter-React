496dea10e86959a050d43885f9322c2435cb44b5
VV <victor@gmail.com>
2020-06-07 16:50:20 +0300

    create a simple counter with React. OOP method

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.sass';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      count: 0,
+    };
+    this.values = [5, -5];
+  }
+
+  onStateChange(i) {
+    this.setState(({ count }) => {
+      return {
+        count: count + i,
+      };
+    });
+  }
+  
+
+  render() {
+    const { count } = this.state;
+
+    return (
+      <div className="App">
+        <div className="uk-card uk-card-default uk-card-body uk-width-1-2@m uk-margin-medium-top my-wrapp">
+          <h3 className="uk-card-title">Counter</h3>
+          <div className="uk-text-large uk-text-bold uk-text-success ">
+            {" "}
+            {count}{" "}
+          </div>
+          <div className="btn-box uk-margin-small-top">
+            <button
+              className="uk-button uk-button-secondary uk-width-1-3"
+              onClick={() => this.onStateChange(
+                +this.values.filter((item) => item > 0)
+              )}
+            >
+              +
+            </button>
+            <button
+              className="uk-button uk-button-primary uk-width-1-3"
+              onClick={() => this.onStateChange(
+                 +this.values.filter((item) => item < 0)
+              )}
+            >
+              -
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
