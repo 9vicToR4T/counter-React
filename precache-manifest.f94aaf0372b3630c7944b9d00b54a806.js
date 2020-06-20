b33e0b67580541eae73e8c7db0cb143320a792c3
VV <victor@gmail.com>
2020-06-20 18:49:39 +0300

    Updates

---
+++ precache-manifest.f94aaf0372b3630c7944b9d00b54a806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cf37de7eecd87211e0fcd458273ed0e",
+    "url": "/counter-React/index.html"
+  },
+  {
+    "revision": "4d678d4bbca1a8e34bf9",
+    "url": "/counter-React/static/css/main.3bd1f837.chunk.css"
+  },
+  {
+    "revision": "f05345fa3e3d8709d349",
+    "url": "/counter-React/static/js/2.884bb6da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter-React/static/js/2.884bb6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d678d4bbca1a8e34bf9",
+    "url": "/counter-React/static/js/main.400888f4.chunk.js"
+  },
+  {
+    "revision": "bde0967d0edc5768c5dd",
+    "url": "/counter-React/static/js/runtime-main.119d1100.js"
+  }
+]);
